3dfb8997f527e4574135aafb2f8c83a19127f1a3
Rellot's Screwdriver <JKanimations71@gmail.com>
2023-05-18 16:32:19 +0530

    First Commit
    Snapshotting the default configs in case if anything goes wrong

---
+++ fetch.sh
@@ -0,0 +1,5 @@
+cp -r ~/.config/{fish,nvim,i3,polybar,btop,lazygit,neofetch,rofi,picom} .
+cp -r ~/{.Xresources,.xinitrc} .
+cp -r /wallpapers .
+#cp -r /etc/lightdm/{lightdm.conf,lightdm-gtk-greeter.conf}
+#cp -r /etc/lightdm/lightdm-gtk-greeter.conf
---
+++ install.sh
@@ -0,0 +1,9 @@
+#installing
+cp -r picom btop nvim polybar i3 fish lazygit rofi neofetch ~/.config
+cp -r .Xresources .xinitrc ~
+
+mkdir ~/.themes ~/.icons
+
+gsettings set org.cinnamon.desktop.default-applications.terminal exec urxvt
+
+

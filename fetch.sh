592e89364453bb47946a4b01ffbdf59d19a5bfca
Rellot's Screwdriver <JKanimations71@gmail.com>
2023-06-17 10:55:00 +0530

    added variety in scripts and only fetch a specific theme

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,8 @@
-cp -r ~/.config/{"gtk-3.0",dunst,nvim,ranger,xfce4,"gtk-2.0",fish,i3,polybar,btop,lazygit,neofetch,rofi,picom} .
+cp -r ~/.config/{"gtk-3.0",dunst,nvim,ranger,xfce4,"gtk-2.0",fish,i3,polybar,btop,lazygit,neofetch,variety,rofi,picom} .
 cp -r ~/.xinitrc .
 cp -r /wallpapers .
-cp -r ~/.themes/ themes
-cp -r ~/.local/share/icons/* icons
+cp -r ~/.themes/Synthwave themes
+cp -r ~/.local/share/icons/McMojave-circle-black icons
 
 #cp -r /etc/lightdm/{lightdm.conf,lightdm-gtk-greeter.conf}
 #cp -r /etc/lightdm/lightdm-gtk-greeter.conf
--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #installing
-cp -r ranger dunst xfce4 "gtk-2.0" "gtk-3.0" picom btop nvim polybar i3 fish lazygit rofi neofetch ~/.config
+cp -r ranger dunst xfce4 "gtk-2.0" "gtk-3.0" picom nvim btop polybar i3 fish lazygit rofi neofetch ~/.config
 cp -r .xinitrc ~
 cp -r icons ~/.local/share/icons/Synthwave/
 cp -r wallpapers /
 mkdir ~/.themes ~/.icons
 
 gsettings set org.cinnamon.desktop.default-applications.terminal exec xfce4-terminal 
-
 

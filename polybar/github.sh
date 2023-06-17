d9cb7458375381716e7b37151fbb82d2b2cefd8e
Rellot's Screwdriver <JKanimations71@gmail.com>
2023-06-17 10:51:50 +0530

    configured polybar and added icons, variety
    polybar:
    + added underline and hover background in workspace icons
    * adjusted polybar's height, padding and margin
    * fixed icons not being aligned
    * fixed github module not being able to show notifications
    - removed unused modules

--- polybar/github.sh
+++ polybar/github.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-USER=""
+USER="rellotscrewdriver"
 
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
-    echo "# $notifications"
+	echo "%{F#FF69B4} %{F-}$notifications"
 else
-    echo ""
+  echo "%{F#FF0000} %{F-}"
 fi

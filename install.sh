2b7bb472079615fa3b485ff45d34aab18005612f
Rellot's Screwdriver <JKanimations71@gmail.com>
2023-09-23 21:40:34 +0530

    updated installation script a bit and lightdm theme

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ while :
 do
 		echo " "
     echo -e  "${Green} [1] Install Configuration?  ${Reset}"
-    echo -e  "${Yellow}[0] Quit        ${Reset}"
+    echo -e  "${Yellow} [0] Quit        ${Reset}"
     echo
     read -p " Enter your choice: " choice
     case $choice in
@@ -49,21 +49,19 @@ do
 				ln -sfvr $DIR/.xinitrc ~/.xinitrc
 
 				#icons, themes and wallpapers
-				mkdir -p ~/.icons/Synthwave ~/.themes/Synthwave/ /wallpapers
-				ln -sfvr $DIR/icons/ ~/.icons/Synthwave/
-				ln -sfvr $DIR/GTK_theme ~/.themes/Synthwave/
+				mkdir -p ~/.icons/catppuccin ~/.themes/catppuccin/ /wallpapers
+				ln -sfvr $DIR/theme/icons/ ~/.icons/catppuccin/
+				ln -sfvr $DIR/theme/GTK_theme ~/.themes/catppuccin/
 				ln -sfvr $DIR/wallpapers /wallpapers
+				chmod -x /wallpapers
 
 				#lightdm and greeter
-				ln -sfvr $DIR/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
-				ln -sfvr $DIR/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-				ln -sfvr $DIR/lightdm/pfp.png /etc/lightdm/pfp.png
+				sudo ln -sfvr $DIR/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
+				sudo ln -sfvr $DIR/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+				sudo ln -sfvr $DIR/lightdm/pfp.png /etc/lightdm/pfp.png
 
-				#greeter theme
-				ln -sfvr $DIR/lightdm/greeter_theme /usr/share/web-greeter/themes/waves
-	
 				#GRUB theme
-				ln -sfvr $DIR/GRUB_theme/Vimix /usr/share/grub/themes
+				ln -sfvr $DIR/theme/GRUB_theme/ /usr/share/grub/themes
 				grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
 				echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
 				grub-mkconfig -o /boot/grub/grub.cfg
@@ -72,11 +70,8 @@ do
         [ ! -d $HOME/.local/share/fonts ] && mkdir -p $HOME/.local/share/fonts
         #ln -sfv $DIR/.local/share/fonts/feather.ttf $HOME/.local/share/fonts/feather.ttf
 				
-				#wallpapers
-				ln -sfvr $DIR/wallpapers /wallpapers
-
 				#Configure betterlockscreen
-				betterlockscreen -u /wallpapers/miami6.jpg 
+				betterlockscreen -u /wallpapers/forrest.png 
 
 				#installing neovim
 				sh $DIR/configs/nvim/install.sh

5ca7cdbb4c740057d897a0d10810ea68c1ce055f
Rellot's Screwdriver <JKanimations71@gmail.com>
2023-09-30 13:08:03 +0530

    configured neofetch, polybar and xfce4-terminal
    neofetch:
    * changed all the colors to blue
    
    polybar:
    * removed the extra description of the weather
    
    xfce4:
    - removed the dock
    * changed the shortcut for opening a new terminal

--- configs/polybar/github.sh
+++ configs/polybar/github.sh
@@ -6,9 +6,9 @@ TOKEN="$(<$HOME/key.txt)"
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
-	notify-send "Github" "You have $notifications new message(s)"
-	#echo "%{F#FF69B4} %{F-}$notifications"
+		#echo "%{F#FF69B4} %{F-}$notifications"
+		notify-send "Github" "You have $notifications new message(s)"
 else
-	notify-send "Github" "You have no new messages"
-  #echo "%{F#FF69B4} %{F-}0"
+    #echo "%{F#FF69B4} %{F-}0"
+		notify-send "Github" "You have no new messages"
 fi

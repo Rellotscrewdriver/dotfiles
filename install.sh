8369a8ce78e852fadc116bfe2f2d7e43e18958ff
Rellot's Screwdriver <Rellotscrewdriver@users.noreply.github.com>
2025-04-28 09:08:21 +0530

    added more options in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # Credits to HoNamDuong
+# I apologize for not using functions because I am not good enough in shell scripting
 ## Directories ----------------------------
 DIR=$(pwd)
+LOCAL_DIR=~/.local/share/fonts 
+NERD_FONT_DIR=~/RobotoMono.zip
 
 ## Directories ----------------------------
 # Reset
@@ -10,31 +13,29 @@ Reset='\033[0m'       # Text Reset
 
 # Regular Colors
 Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
 
 # Bold
 BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
 BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
 BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
 
 clear
 
+echo -e "${Yellow}               Welcome to my configuration!"
+echo "Select anything you want and it will link to destinated folder"
+echo "after installing do not delete this folder the configs are in, else it won't work properly"
 while :
 do
 		echo " "
-    echo -e  "${Green} [1] Install Configuration?  ${Reset}"
-    echo -e  "${Yellow}[0] Quit        ${Reset}"
+    echo -e  "${Yellow} [1] Install Everything"
+    echo -e  "${Cyan} [2] Install Theme       "
+    echo -e  "${Yellow} [3] Install Icons "
+    echo -e  "${Cyan} [4] Create Token "
+    echo -e  "${Yellow} [5] Install Wallpapers"
+    echo -e  "${Cyan} [6] Install Config Files(Sway, Swaylock, btop etc.)"
+    echo -e  "${Yellow} [0] Quit        ${Reset}"
     echo
     read -p " Enter your choice: " choice
     case $choice in
@@ -43,44 +44,101 @@ do
         ;;
     1)
 				#configs
+                echo "Installing configs"
 				ln -sfvr $DIR/configs/* ~/.config/
+				
+                echo "copying wlogout icons(this wouldn't work unless you're in sudo mode)"
+                sudo mkdir /usr/share/wlogout/icons/
+                sudo ln -sfvr $DIR/configs/wlogout/icons/* /usr/share/wlogout/icons/
 
-				#init filwe
-				ln -sfvr $DIR/.xinitrc ~/.xinitrc
+                echo "setting permissions for github.sh and weather.sh"
+				chmod +x $DIR/configs/waybar/github.sh
+				chmod +x $DIR/configs/waybar/weather.sh
 
-				#icons, themes and wallpapers
+                echo "creating a token"
+                touch ~/.config/waybar/token
+                read -p "Enter the github token: " token
+                echo $token > ~/.config/waybar/token
+
+				echo "installing icons theme and wallpapers"
 				mkdir -p ~/.icons/Synthwave ~/.themes/Synthwave/ /wallpapers
 				ln -sfvr $DIR/icons/ ~/.icons/Synthwave/
 				ln -sfvr $DIR/GTK_theme ~/.themes/Synthwave/
 				ln -sfvr $DIR/wallpapers /wallpapers
-
-				#lightdm and greeter
-				ln -sfvr $DIR/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
-				ln -sfvr $DIR/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-				ln -sfvr $DIR/lightdm/pfp.png /etc/lightdm/pfp.png
-
-				#greeter theme
-				ln -sfvr $DIR/lightdm/greeter_theme /usr/share/web-greeter/themes/waves
 	
-				#GRUB theme
-				ln -sfvr $DIR/GRUB_theme/Vimix /usr/share/grub/themes
-				grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
-				echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
-				grub-mkconfig -o /boot/grub/grub.cfg
-
 				#font
-        [ ! -d $HOME/.local/share/fonts ] && mkdir -p $HOME/.local/share/fonts
-        #ln -sfv $DIR/.local/share/fonts/feather.ttf $HOME/.local/share/fonts/feather.ttf
-				
+                echo "installing fonts"
+                echo "Installing RobotMono nerd font in your local directory"
+                mkdir -p $LOCAL_DIR
+                if [ -f $NERD_FONT_DIR ]; then
+                    echo "RobotoMono's font's already there, skip installing it"
+                else
+                    echo "installing the required fonts from the internet, please wait if you do have internet connection"
+                    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/RobotoMono.zip -O $NERD_FONT_DIR
+                fi
+
+                unzip -j $NERD_FONT_DIR -d $LOCAL_DIR
+                fc-cache -f
+                rm -f $NERD_FONT_DIR
+
 				#wallpapers
-				ln -sfvr $DIR/wallpapers /wallpapers
+				ln -sfvr $DIR/wallpapers ~/wallpapers
+        break                
+    ;;
+    2)	    
+        echo "installing theme"
+        mkdir -p ~/.themes/Synthwave/
+		ln -sfvr $DIR/GTK_theme ~/.themes/Synthwave/
+        break
+    ;;
+    3)
+        echo "installing icons"
+	    mkdir -p ~/.icons/Synthwave
+		ln -sfvr $DIR/icons/ ~/.icons/Synthwave/
+        break
+    ;;
+    4)
+        echo "creating a token"
+        touch ~/.config/waybar/token
+        read -p "Enter the github token: " token
+        echo $token > ~/.config/waybar/token
+        break
+    ;;
+    5)
+        echo "Creating symlink to wallpapers"
+        mkdir ~/wallpapers
+	    ln -sfvr $DIR/wallpapers ~/wallpapers
+        break
+    ;;
+    6)
+        echo "Installing configs"
+		ln -sfvr $DIR/configs/* ~/.config/
+				
+        echo "copying wlogout icons(this wouldn't work unless you're in sudo mode)"
+        sudo mkdir /usr/share/wlogout/icons/
+        ln -sfvr $DIR/configs/wlogout/icons/* /usr/share/wlogout/icons/
 
-				#Configure betterlockscreen
-				betterlockscreen -u /wallpapers/miami6.jpg 
+        echo "setting permissions for github.sh and weather.sh"
+		chmod +x $DIR/configs/waybar/github.sh
+		chmod +x $DIR/configs/waybar/weather.sh
 
-				#installing neovim
-				sh $DIR/configs/nvim/install.sh
-        ;;
+		#font
+        echo "installing fonts"
+        echo "Installing RobotMono nerd font in your local directory"
+        mkdir -p $LOCAL_DIR
+        if [ -f $NERD_FONT_DIR ]; then
+            echo "RobotoMono's font's already there, skip installing it"
+        else
+            echo "installing the required fonts from the internet, please wait if you do have internet connection"
+            wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/RobotoMono.zip -O $NERD_FONT_DIR
+        fi
+
+        unzip -j $NERD_FONT_DIR -d $LOCAL_DIR
+        fc-cache -f
+        rm -f $NERD_FONT_DIR
+
+        break
+    ;;    
     *)
         echo "Sorry, choice understand"
         ;;

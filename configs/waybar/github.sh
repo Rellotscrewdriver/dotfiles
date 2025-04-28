7b6a5a588007c9c6604e5d8db1ec195d94c900b4
Rellot's Screwdriver <Rellotscrewdriver@users.noreply.github.com>
2025-04-28 09:07:55 +0530

    adjusted some configs

--- configs/waybar/github.sh
+++ configs/waybar/github.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+user="rellotscrewdriver"
 token=`cat ~/.config/waybar/token`
-count=`curl -u username:${token} https://api.github.com/notifications | jq '. | length'`
+#count=`curl -u "rellotscrewdriver":${token} https://api.github.com/notifications | jq '. | length'`
+count=$(echo "user = \"$user:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 if [[ "$count" != "0" ]]; then
-    echo '{"text":'$count',"tooltip":"$tooltip","class":"$class"}'
+    echo '{"text":'$count',"tooltip":"you have '$count' notifications","class":"$class"}'
+else
+    echo '{"text":"0","tooltip":"you have 0 notifications","class":"$class"}'
 fi

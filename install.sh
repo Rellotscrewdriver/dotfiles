3abb533d50cf9dbfca71129dbe2d1ed56acc554c
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2026-02-04 17:22:28 +0530

    test17

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ installGRUB(){
 installLogin(){
     echo "installing login theme"
 	sudo cp -r $DIR/theme/login/ /etc/greetd/*
+    sudo cp -r $DIR/wallpapers/Night.png /usr/share/backgrounds/greeter.jpg
     sudo systemctl disable display-manager
     sudo systemctl enable greetd
     echo "done"
